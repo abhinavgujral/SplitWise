6c99f43217cb50f491287985bcded99bc298e62d
ShubhamShegokar1 <shubhamsandy.1@gmail.com>
2021-12-16 15:23:43 +0530

    update

---
+++ src/main/java/com/masai/app/book_review/Exception/ExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.masai.app.book_review.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+    public final ResponseEntity<Object> userNotFound (Exception e, WebRequest request){
+        ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
+        return  new ResponseEntity<>(exceptionResponse, HttpStatus.ACCEPTED);
+    }
+
+}
