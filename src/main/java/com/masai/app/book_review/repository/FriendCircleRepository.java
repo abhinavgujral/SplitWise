b786a88b85b40ed0185dc121d5ad4cddf6e1cd99
Nikhil Rane <70564188+Nikhil-Rane-Lab@users.noreply.github.com>
2021-12-16 13:29:19 +0530

    Delete src directory

--- src/main/java/com/masai/app/book_review/repository/FriendCircleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.masai.app.book_review.repository;
-
-import com.masai.app.book_review.entity.FriendCircle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FriendCircleRepository extends JpaRepository<FriendCircle, String>
-{
-}
